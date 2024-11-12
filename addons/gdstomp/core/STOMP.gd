c040a5ed083b0a486b2e7cc2e620f896ab28fca6
Scrawach <Scrawach@gmail.com>
2024-11-12 11:08:49 +0300

    allowed inject handshake headers for webscoket connection from STOMP factory method

--- addons/gdstomp/core/STOMP.gd
+++ addons/gdstomp/core/STOMP.gd
@@ -4,5 +4,5 @@ extends RefCounted
 static func over_tcp() -> STOMPClient:
 	return STOMPClient.new(TcpSTOMPConnection.new())
 
-static func over_websockets() -> STOMPClient:
-	return STOMPClient.new(WebSTOMPConnection.new())
+static func over_websockets(handshake_headers: PackedStringArray = [], tls: TLSOptions = null) -> STOMPClient:
+	return STOMPClient.new(WebSTOMPConnection.new(handshake_headers, tls))
